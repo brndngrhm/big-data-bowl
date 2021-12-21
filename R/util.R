bbdf06a0c9c819cb155a9e359fd507e4f5afa9a4
brndngrhm <brndngrhm@users.noreply.github.com>
2021-12-21 17:00:35 -0500

    edit load data function

--- R/util.R
+++ R/util.R
@@ -13,12 +13,30 @@ add_table <-
                            showSortIcon = T, striped = T, compact = T, defaultExpanded = T)
   }
 
-load_data <- 
+load_clean_data <- 
   function(file_name){
-    feather::read_feather(here("data", {{file_name}}))
+    feather::read_feather(here("data", "clean", {{file_name}}))
   }
 
-load_tracking_data <- 
+load_raw_data <- 
   function(file_name){
-    feather::read_feather(here("data", "tracking", {{file_name}}))
+    feather::read_feather(here("data", "raw", {{file_name}}))
+  }
+
+load_anaylsis_data <- 
+  function(file_name, type){
+    if(!type %in% c("raw", "clean", "analysis")) stop('type must be one of "raw", "clean", "analysis"')
+    
+    feather::read_feather(here("data", {{type}}, {{file_name}}))
+  }
+
+load_data <- 
+  function(type, file_name){
+    if(!type %in% c("raw", "clean", "analysis")) stop('type must be one of "raw", "clean", "analysis"')
+    feather::read_feather(here("data", {{type}}, {{file_name}}))
+  }
+
+add_big_labels <- 
+  function(text_size = 14){
+    text = element_text(size = text_size)
   }
